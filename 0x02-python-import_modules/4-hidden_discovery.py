0426d4c662231a1e5094d34c7aa494c1e4e11572
hachimB <boubacarhachim@gmail.com>
2023-09-07 17:00:02 +0100

    Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import hidden_4
-    names = dir(hidden_4)
+    import importlib.util
+    # Specify the file path to the compiled module
+    module_name = "hidden_4"
+    module_path = f"{module_name}.pyc"
+    # Create a module spec from the file location
+    spec = importlib.util.spec_from_file_location(module_name, module_path)
+    # Load the module from the spec
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    names = dir(module)
     names.sort()
     for item in names:
         if not item.startswith("__"):
