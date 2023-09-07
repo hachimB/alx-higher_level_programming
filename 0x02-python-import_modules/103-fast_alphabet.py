331e9516f097f9d5cc345205c26492acaf153302
hachimB <boubacarhachim@gmail.com>
2023-09-07 23:34:35 +0100

    Fast alphabet

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-from string import ascii_uppercase
-exec("from sys import stdout; stdout.write(ascii_uppercase + '\\n')")
+from string import ascii_uppercase;
+from sys import stdout; stdout.buffer.write(bytes(range(65, 91)) + b'\n')
+#exec("from sys import stdout; stdout.write(ascii_uppercase + '\\n')")
