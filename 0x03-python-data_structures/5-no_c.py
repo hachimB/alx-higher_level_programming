ab1630330e06712483939171708df8d69201c244
hachimB <boubacarhachim@gmail.com>
2023-09-09 15:54:54 +0100

    Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     new_str = ""
     for chars in my_string:
-        if (chars != chr(67) and chars != chr(99)):
+        if ((chars != chr(67)) and (chars != chr(99))):
             new_str = new_str + chars
-    return(new_str)
+    return (new_str)
