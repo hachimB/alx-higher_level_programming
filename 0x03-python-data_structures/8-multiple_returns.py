ce6bc51bf2cf4db4ce4b7c17fe5761967461a3cb
hachimB <boubacarhachim@gmail.com>
2023-09-10 23:09:18 +0100

    More returns!

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    first_c = sentence[0]
-    if (length == 0):
-        first_c = None
+    first_c = None
+    if (length > 0):
+        first_c = sentence[0]
     tuple = (length, first_c)
     return (tuple)
