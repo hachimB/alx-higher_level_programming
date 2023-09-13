15f0babb2213cecf10d4e29dc55f0b6f3a5fa813
hachimB <boubacarhachim@gmail.com>
2023-09-13 10:45:40 +0100

    Search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if (my_list is None or search is None or replace is None):
-        return
     new_list = [num for num in my_list]
-    for i in range(len(new_list) - 1):
+    for i in range(len(new_list)):
         if (new_list[i] == search):
             new_list[i] = replace
     return (new_list)
