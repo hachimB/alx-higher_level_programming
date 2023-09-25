12b127797f1853bdc7f95a0e4c2048b8d9dda483
hachimB <boubacarhachim@gmail.com>
2023-09-25 10:32:26 +0100

    safe

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,21 +2,13 @@
 def safe_print_list(my_list=[], x=0):
     try:
         count = 0
-        list2 = []
         for c in my_list:
             count += 1
         if (x > count):
-            for i in my_list:
-                print(i, end="")
-            print()
-        else:
-            for i in my_list:
-                list2 = my_list[:x]
-            for i in list2:
-                print(i, end="")
-            print()
-        if (count == 0):
-            i = 0
-        return (i)
+            x = count
+        for i in my_list[:x]:
+            print(i, end="")
+        print()
+        return (x)
     except Exception as e:
         print(e)
