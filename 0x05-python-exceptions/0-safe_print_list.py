a659d42d28ad376df59277c2a54540f76f183d3e
hachimB <boubacarhachim@gmail.com>
2023-09-25 10:22:18 +0100

    safe

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -15,8 +15,6 @@ def safe_print_list(my_list=[], x=0):
             for i in list2:
                 print(i, end="")
             print()
-        if (count == 0):
-            i = 0
         return (i)
     except Exception as e:
         print(e)
