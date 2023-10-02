43589aaa994504e08eb6e5dcafbb16d58f7f00ba
hachimB <boubacarhachim@gmail.com>
2023-10-02 12:30:56 +0100

    String representation

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""class documentation"""
+
+
+class Rectangle:
+    """class of Rectangle"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if (value < 0):
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if (value < 0):
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return (self.width * self.height)
+
+    def perimeter(self):
+        if (self.width == 0 or self.height == 0):
+            perim = 0
+        else:
+            perim = (self.width + self.height) * 2
+        return (perim)
+
+    def __str__(self):
+        str = ""
+        if (self.width == 0 or self.height == 0):
+            str = str
+        for i in range(self.height):
+            for i in range(self.width):
+                str += "#"
+            str += "\n"
+        return(str)
