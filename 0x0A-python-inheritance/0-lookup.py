04594995ef57a087ce5b3a8d2b24633f2df3ba12
hachimB <boubacarhachim@gmail.com>
2023-10-09 18:39:12 +0100

    Lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module documentation"""
+
+
+def lookup(obj):
+    """This is the lookup function that returns the list of available attributes
+ and methods of an object"""
+    return (dir(obj))
