a4e3b33641a6c0d6f03d7e5fc7206c76a484989a
hachimB <boubacarhachim@gmail.com>
2023-10-11 12:11:15 +0100

    Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-"""Module documentaion"""
+"""That's the module documentaion"""
 
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+try:
+    m_list = load_from_json_file("add_item.json")
+except Exception:
+    m_list = []
 
-m_list = load_from_json_file("add_item.json")
 for i in sys.argv:
     if not (i == sys.argv[0]):
         m_list.append(i)
