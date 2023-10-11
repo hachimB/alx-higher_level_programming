f543a4e74d12aed2e293507f6311241cf9cb39ac
hachimB <boubacarhachim@gmail.com>
2023-10-11 11:47:43 +0100

    Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,6 +5,7 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
+"""here the code"""
 m_list = load_from_json_file("add_item.json")
 for i in sys.argv:
     if not (i == sys.argv[0]):
