d57b4f9f23a3a99c9dc47e2252658d2e93834754
hachimB <boubacarhachim@gmail.com>
2023-10-15 18:18:05 +0100

    OK

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -26,6 +26,12 @@ class Base:
             list_objs = []
         else:
             list_objs = [obj.to_dictionary() for obj in list_objs]
-        with open("{}.json".format(cls.__name__), "w", encoding="utf8")
-        as file:
+        with open("{}.json".format(cls.__name__), "w",
+                  encoding="utf8") as file:
             file.write(cls.to_json_string(list_objs))
+
+    @staticmethod
+    def from_json_string(json_string):
+        if (not json_string or json_string is None):
+            return []
+        return json.loads(json_string)
