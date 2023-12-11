8cd7591594096b5bb890c85bbccf4ba3cc57b309
hachimB <boubacarhachim@gmail.com>
2023-12-11 23:47:58 +0100

    ok

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+let i;
+const arr = process.argv.slice(2);
+console.log(arr);
+for (i = 0; i < arr.length; i++) {
+  arr[i] = Number(arr[i]);
+}
+// sorting in descending order
+arr.sort(function (a, b) {
+  return b - a;
+});
+
+if (arr.length < 2) {
+  console.log(0);
+} else {
+  console.log(arr[1]);
+}
