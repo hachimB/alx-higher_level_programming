4df1080068d0e813f82f27ccd2026ae885d433d8
hachimB <boubacarhachim@gmail.com>
2023-12-11 17:36:21 +0100

    isNaN

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-if (!process.argv[2] || !/\d/.test(process.argv[2])) {
+if (!process.argv[2] || isNaN(process.argv[2])) {
   console.log('Not a number');
 } else {
-  console.log(Number(process.argv[2]));
+  console.log('My number: ' + Number(process.argv[2]));
 }
