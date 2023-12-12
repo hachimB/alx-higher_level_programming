ab941387ccb7222e48ba40e58b97616361a7f4c4
hachimB <boubacarhachim@gmail.com>
2023-12-12 21:33:10 +0100

    Rectangle #2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0) { return {}; }
-    this.width = w;
-    this.height = h;
+    // if (w <= 0 || h <= 0) { return {}; }
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else { return {}; }
   }
 }
 module.exports = Rectangle;
