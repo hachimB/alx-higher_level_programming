b82fe539ef22c442835d4a084563bc2aefeaa6fc
hachimB <boubacarhachim@gmail.com>
2023-12-15 15:54:35 +0100

    try

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module documentation"""
+import sqlalchemy
+from sqlalchemy.ext.declarative import declarative_base
+import sys
+
+
+Base = declarative_base()
+
+class State(Base):
+    """State class"""
+    __tablename__ = 'states'
+    id = sqlalchemy.Column(sqlalchemy.Integer, autoincrement=True, unique=True,
+                primary_key=True, nullable=False)
+    name = sqlalchemy.Column(sqlalchemy.String(128), nullable=False)
+
+engine = sqlalchemy.create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                                  format(sys.argv[1], sys.argv[2], sys.argv[3]))
+Base.metadata.create_all(engine)
