7e06cbd6e6fbc11c1a14ee8a8455522cbff2081f
hachimB <boubacarhachim@gmail.com>
2023-12-15 16:00:22 +0100

    try

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -13,6 +13,3 @@ class State(Base):
     id = sqlalchemy.Column(sqlalchemy.Integer, autoincrement=True, unique=True,
                 primary_key=True, nullable=False)
     name = sqlalchemy.Column(sqlalchemy.String(128), nullable=False)
-
-#engine = sqlalchemy.create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-#                                 format(sys.argv[1], sys.argv[2], sys.argv[3]))
