d3cf37344ee20160ad5435c66435597e57db8e11
hachimB <boubacarhachim@gmail.com>
2023-12-15 22:50:51 +0100

    ok

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Module documentation"""
 from sqlalchemy import (create_engine)
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
