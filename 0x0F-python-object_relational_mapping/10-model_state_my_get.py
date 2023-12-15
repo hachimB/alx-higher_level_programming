a0203ed21e49adf9ac521ab5e5789ac56d6134e6
hachimB <boubacarhachim@gmail.com>
2023-12-15 23:55:44 +0100

    OK

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module documentation"""
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+import sys
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        sys.exit(1)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state_count = session.query(State).filter(State.name.like(sys.argv[4])).order_by(State.id).count()
+    if (state_count == 0):
+        print(Not found)
+    else:
+        print(state_count)
