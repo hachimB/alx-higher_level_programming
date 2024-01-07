ed445e24384ea22e519e66414144a3b006b0fa66
hachimB <boubacarhachim@gmail.com>
2024-01-07 19:42:38 +0100

    README

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     with urllib.request.urlopen("https://alx-intranet.hbtn.io/status")\
             as response:
         content = response.read()
-        utf8_content = content.decode("utf-8")
+    utf8_content = content.decode("utf-8")
     print("Body response:")
     print("    - type: ", type(content))
     print("    - content: ", content)
