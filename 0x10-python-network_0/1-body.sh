1bc78c5b23ee9a488d01d31cdffa4d8f09205125
hachimB <boubacarhachim@gmail.com>
2024-01-07 13:18:59 +0100

    ok

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -sL -w "%{code}" "$1" -o /dev/null | grep -q "200" && curl -sL "$1"
+curl -sL -w "%{http_code}" "$1" -o /dev/null | grep -q "200" && curl -sL "$1"
