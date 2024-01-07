140b1bb277073056f20d4bd26957517351de9119
hachimB <boubacarhachim@gmail.com>
2024-01-07 11:24:56 +0100

    ok

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes in a URL, sends a request to that URL, and displays the size of the body of the response
-response_size = $(curl -sI "$1" | grep -i 'Content-Length' | awk '{print $1}')
+curl -s -w "%{size_download}" -o /dev/null "$1"
