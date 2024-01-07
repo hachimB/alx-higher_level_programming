a511f743ae4b8a74d7769278aa5f10a682a82717
hachimB <boubacarhachim@gmail.com>
2024-01-07 23:33:08 +0100

    ok

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,7 +8,10 @@ if __name__ == '__main__':
     with urllib.request.urlopen(url) as response:
         content = response.read()
         utf8_content = content.decode('utf-8')
-        display = "Body response:\n\t- type: {}\n\t- content: {}\
-                \n\t- utf8 content: {}".format(
-                        type(content), content, utf8_content)
+        display = (
+                "Body response:\n"
+                "\t- type: {}\n"
+                "\t- content: {}\n"
+                "\t- utf8 content: {}"
+                ).format(type(content), content, utf8_content)
     print(display)
