8d684964e933931bae72648ecbccbedb82e7d8b2
hachimB <boubacarhachim@gmail.com>
2024-01-07 19:20:44 +0100

    README

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,8 +8,7 @@ if __name__ == '__main__':
             as response:
         content = response.read()
         utf8_content = content.decode("utf-8")
-    display = """Body response:
-    - type: {}
-    - content: {}
-    - utf8 content: {}""".format(type(content), content, utf8_content)
-    print(display)
+    print("Body response:")
+    print("    - type:", type(content))
+    print("    - content:", content)
+    print("    - utf8 content:", utf8_content)
